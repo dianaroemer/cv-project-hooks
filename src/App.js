33c7e7b8ef0e21bd391f954b7fa4d37784220e73
dominicroemer <dominicroemer@gmail.com>
2022-07-17 15:16:09 -0700

    Begin fleshing out FormContainer and PersonalInfoForm.js to functional components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+import FormContainer from './Components/FormContainer';
 
 function App() {
+  
+  const [personalInformation, setPersonalInformation] = useState({
+    name : '',
+    title: '',
+    photoURL: '',
+    address: '',
+    phone: '',
+    email: '',
+    linkedIn: '',
+    gitHub: '',
+    type: 'personalInformation'
+  });
+
+  function handlePersonalInformation(e, targetObject, targetField) {
+    setPersonalInformation({...targetObject, [targetField]: e.target.value});
+  };
+
+  
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="AppContainer" style={{backgroundColor: 'bisque'}}>
+      {/* <NavBar/> */}
+      <div className='content'>
+        <FormContainer
+          personalInformation={personalInformation}
+          handlePersonalInformation={handlePersonalInformation}
+        />
+        
+        {/* <PreviewContainer className='previewContainer'
+        appState={this.state}/> */}
+
+      </div>
+
+
     </div>
   );
 }
---
+++ src/Components/FormContainer.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+// import FormElement from './FormElement';
+import PersonalInformationForm from './Field Components/PersonalInformationForm';
+import WorkExperienceForm from './Field Components/WorkExperienceForm'
+import SkillForm from './Field Components/SkillForm'
+import EducationalExperienceForm from './Field Components/EducationalExperienceForm';
+
+
+// class FormContainer extends Component {
+//     // constructor(props) {
+//         // super(props);
+//     // }
+
+//     render() {
+//         const { updateInputField, appState, createExperience, deleteExperience, toggleSkillEdit, setSampleState } = this.props;
+
+
+//         return (
+//             <div className='FormContainer'>
+//                 {/* <h3>I am FormContainer.js. I contain forms. Let's put something together</h3> */}
+//                 <button type="submit" onClick={setSampleState} className='sampleStateButton'>Generate Sample CV</button>
+//                 <PersonalInformationForm 
+//                     updateInputField={updateInputField}
+//                     personalInformation={appState.personalInformation}/>
+//                 <WorkExperienceForm workExperienceArr={appState.workExperience.workExperienceArr}
+//                     elementType={appState.workExperience.type}
+//                     updateInputField={updateInputField}
+//                     createExperience={createExperience}
+//                     deleteExperience={deleteExperience}/>
+                
+//                 <SkillForm skillArr={appState.skillArr}
+//                 createExperience={createExperience}
+//                 deleteExperience={deleteExperience}
+//                 toggleSkillEdit={toggleSkillEdit}
+//                 updateInputField={updateInputField}/>
+                
+//                 <EducationalExperienceForm
+//                     educationalExperienceArr={appState.educationalExperience.educationalExperienceArr}
+//                     elementType={appState.educationalExperience.type}
+//                     updateInputField={updateInputField}
+//                     createExperience={createExperience}
+//                     deleteExperience={deleteExperience}/>
+
+//                     {/* <FormElement/> */}
+//                 {/* <FormElement/> */}
+//             </div>
+//         )
+//     }
+// }
+
+function FormContainer(props) {
+
+    const { personalInformation, handlePersonalInformation } = props;
+
+    return (
+        <div className='FormContainer'>
+            {/* <h3>I am FormContainer.js. I contain forms. Let's put something together</h3> */}
+            <button type="submit" onClick={e => e.preventDefault()} className='sampleStateButton'>Generate Sample CV</button>
+            <PersonalInformationForm 
+                handlePersonalInformation={handlePersonalInformation}
+                personalInformation={personalInformation}/>
+                
+            {/* <WorkExperienceForm workExperienceArr={appState.workExperience.workExperienceArr}
+                elementType={appState.workExperience.type}
+                updateInputField={updateInputField}
+                createExperience={createExperience}
+                deleteExperience={deleteExperience}/> */}
+            
+            {/* <SkillForm skillArr={appState.skillArr}
+            createExperience={createExperience}
+            deleteExperience={deleteExperience}
+            toggleSkillEdit={toggleSkillEdit}
+            updateInputField={updateInputField}/> */}
+            
+            {/* <EducationalExperienceForm
+                educationalExperienceArr={appState.educationalExperience.educationalExperienceArr}
+                elementType={appState.educationalExperience.type}
+                updateInputField={updateInputField}
+                createExperience={createExperience}
+                deleteExperience={deleteExperience}/> */}
+
+        </div>
+    )
+    
+}
+
+
+
+export default FormContainer;
