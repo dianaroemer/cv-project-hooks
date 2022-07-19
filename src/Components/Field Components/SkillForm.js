6c9a1c707f56601a4e33af30d551dc0f14a81e23
dominicroemer <dominicroemer@gmail.com>
2022-07-18 17:29:42 -0700

    Convert SkillForm to functional component

--- src/Components/Field Components/SkillForm.js
+++ src/Components/Field Components/SkillForm.js
@@ -1,78 +1,64 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash, faPenToSquare, faCircleCheck } from '@fortawesome/free-solid-svg-icons'
 
 
+function SkillForm(props){
 
-class SkillForm extends Component {
+    const {skills,
+        handleUpdateExperience, 
+        handleDeleteExperience, 
+        handleCreateExperience } = props;
 
-    render() {
+    const skillRows = [];
 
-        const {skills,
-            handleUpdateExperience, 
-            handleDeleteExperience, 
-            handleCreateExperience } = this.props;
-
-        const skillRows = [];
-
-        skills.forEach(element => {
-            skillRows.push(
-                <SkillSlot skill={element}
-                handleUpdateExperience={handleUpdateExperience}
-                // toggleSkillEdit={toggleSkillEdit}
-                handleDeleteExperience={handleDeleteExperience}
-                key={element.key}/>
-            )
-        })
-        
-
-        return (
-            <div className='skillForm' style={{borderStyle: 'solid', borderRadius: 2, borderColor: 'red', borderWidth: 10}}>
-                <h3 className='skillsHeaderText'>Skills</h3>
-                {skillRows}
-                
-            <button type="submit" onClick={e => handleCreateExperience(e, 'skills')} id='skillAddButton'>Add Skills</button>
-            <p></p>
-        </div>
+    skills.forEach(element => {
+        skillRows.push(
+            <SkillSlot skill={element}
+            handleUpdateExperience={handleUpdateExperience}
+            handleDeleteExperience={handleDeleteExperience}
+            key={element.key}/>
         )
-    }
+    })
+    
+    return (
+        <div className='skillForm' style={{borderStyle: 'solid', borderRadius: 2, borderColor: 'red', borderWidth: 10}}>
+            <h3 className='skillsHeaderText'>Skills</h3>
+            {skillRows}
+            
+        <button type="submit" onClick={e => handleCreateExperience(e, 'skills')} id='skillAddButton'>Add Skills</button>
+        <p></p>
+    </div>
+    )
 }
 
-class SkillSlot extends Component {
 
-    // constructor(props) {
-    //     super(props);
-    // }
+function SkillSlot(props){
 
-    render() {
+    const { skill,
+        handleUpdateExperience, 
+        handleDeleteExperience } = props;
 
-        const { skill,
-            handleUpdateExperience, 
-            // toggleSkillEdit, 
-            handleDeleteExperience } = this.props;
+    if(skill.edit) {
+        return (
+            <form className='skillEditForm'>
+                <input type="text" className="inputField" placeholder="Shooting womp rats in Beggar's Canyon"  
+                    onChange={e => handleUpdateExperience(e, skill, 'skill', 'skills')} 
+                    onKeyDown={e=> {
+                        if(e.key === "Enter"){
+                            e.preventDefault();
+                            handleUpdateExperience(e, skill, 'edit', 'skills');
+                        }
+                    }}
+                    value={skill.skill}></input>
 
-        if(skill.edit) {
-            return (
-                <form className='skillEditForm'>
-                    <input type="text" className="inputField" placeholder="Shooting womp rats in Beggar's Canyon"  
-                        onChange={e => handleUpdateExperience(e, skill, 'skill', 'skills')} 
-                        onKeyDown={e=> {
-                            if(e.key === "Enter"){
-                                // toggleSkillEdit(e, skill);
-                                e.preventDefault();
-                                handleUpdateExperience(e, skill, 'edit', 'skills');
-                            }
-                        }}
-                        value={skill.skill}></input>
+                <FontAwesomeIcon icon={faCircleCheck} onClick={(e) => 
+                    handleUpdateExperience(e, skill, 'edit', 'skills')}/>
+                <FontAwesomeIcon icon={faTrash} onClick={e=> handleDeleteExperience(e, skill, 'skills')} />
 
-                    <FontAwesomeIcon icon={faCircleCheck} onClick={(e) => 
-                        handleUpdateExperience(e, skill, 'edit', 'skills')}/>
-                    <FontAwesomeIcon icon={faTrash} onClick={e=> handleDeleteExperience(e, skill, 'skills')} />
-
-                </form>
-            );
-        }
-
+            </form>
+        );
+    } else {
         return (
             <form className='skillForm'>
                 {skill.skill} |
@@ -83,7 +69,6 @@ class SkillSlot extends Component {
             </form>
         )
     }
-
 }
 
 export default SkillForm;
